36e899af03a71b4df068cd58837c1d4c3fa5e027
Silver <testingbugsrus@gmail.com>
2023-06-29 21:39:12 +0300

    Change test from appium to selene

--- browserstack_mobil/tests/browserstack_tests.py
+++ browserstack_mobil/tests/browserstack_tests.py
@@ -1,10 +1,8 @@
-import time
-
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selene import have
+from selene.support.shared import browser
 
 options = UiAutomator2Options().load_capabilities({
     # Specify device and os_version for testing
@@ -27,24 +25,13 @@ options = UiAutomator2Options().load_capabilities({
     }
 })
 
-# Initialize the remote Webdriver using BrowserStack remote URL
-# and options defined above
-driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
 
-# Test case for the BrowserStack sample Android app.
-# If you have uploaded your app, update the test case here.
-search_element = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
-)
-search_element.click()
-search_input = WebDriverWait(driver, 30).until(
-    EC.element_to_be_clickable(
-        (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text"))
-)
-search_input.send_keys("BrowserStack")
-time.sleep(5)
-search_results = driver.find_elements(AppiumBy.CLASS_NAME, "android.widget.TextView")
-assert (len(search_results) > 0)
+browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+
+browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("BrowserStack")
+
+browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
 
 # Invoke driver.quit() after the test is done to indicate that the test is completed.
-driver.quit()
+browser.quit()
