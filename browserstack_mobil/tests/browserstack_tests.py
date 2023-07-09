5c140316fb6e75dbc027a18dbc0c86f79acc1e92
Silver <testingbugsrus@gmail.com>
2023-07-09 13:50:22 +0300

    - Add test test_search_other_word

--- browserstack_mobil/tests/browserstack_tests.py
+++ browserstack_mobil/tests/browserstack_tests.py
@@ -1,11 +1,25 @@
+import allure
+from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import have
 from selene.support.shared import browser
 
 
+@allure.step('Открыть страницу википедии')
 def test_search():
-    browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+    with step('Search word BrowserStack'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("BrowserStack")
 
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("BrowserStack")
+    with step('Content should be found'):
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
 
-    browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
+
+@allure.step('Открыть страницу википедии')
+def test_search_other_word():
+    with step('Search word Mars'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Mars")
+
+    with step('Content should be found'):
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
